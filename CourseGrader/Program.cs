a120a12ad0586fe086aad5044034aa551ce4df18
Jessie Monson <jessiemonson1980@gmail.com>
2019-03-13 02:56:56 -0500

    final code

--- CourseGrader/Program.cs
+++ CourseGrader/Program.cs
@@ -6,11 +6,47 @@ namespace CourseGrader
     {
         public static void Main(string[] args)
         {
-            // create a method named GradeTestScores that takes a int array of test scores as its parameter and returns a string
-            // GradeTestScores will add up these test scores and calculate an average score. 
-            // It should then return a string of "pass" or "fail" depending on these two conditions:
-            // If the average score is greater than or equal to 70 and no single test score is below 50, then return a message of "pass".
-            // If the average score is lower than 70 or at least one test score is below 50, then return a message of "fail".
+            int[] testScores = new int[5] { 88, 97, 83, 48, 70 };
+
+            string passFail = GradeTestScores(testScores);
+            Console.WriteLine(passFail);
+            Console.ReadLine();
+        }
+
+        public static string GradeTestScores(int[] testScores)
+        {
+            string pass;
+            int sum = 0;
+            if (testScores == null || testScores.Length == 0)
+                return "fail";
+            foreach (int score in testScores)
+            {
+                sum += score;
+            }
+
+            int average = sum / testScores.Length;
+            if (average >= 70)
+            {
+                pass = "pass";
+            }
+            else if (average < 70)
+            {
+                pass = "fail";
+            }
+            else
+            {
+                pass = "fail";
+            }
+            for (int i = 0; i < testScores.Length; i++)
+            {
+                if (testScores[i] < 50)
+                    pass = "fail";
+            }
+            return pass;
         }
     }
 }
+
+
+        
+      
